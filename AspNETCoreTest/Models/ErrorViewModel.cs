7b9d1467c327b323c3e816001ca5a6af14fcabe6
Cris Vega <crisvega71@gmail.com>
2021-03-15 01:24:45 +0800

    Add project files.

---
+++ AspNETCoreTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNETCoreTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
